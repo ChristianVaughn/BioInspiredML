69cd086372f69a2f3100d0e589f882b478cab072
ChristianVaughn <cvaughn55@gmail.com>
2021-02-21 14:38:43 -0800

    Possible Starting point

---
+++ main.py
@@ -0,0 +1,5 @@
+import random, numpy, math, copy, matplotlib.pyplot as plt
+cities = [random.sample(range(100), 2) for x in range(50)];
+#print(cities)
+tour = random.sample(range(50),50);
+print(tour)
